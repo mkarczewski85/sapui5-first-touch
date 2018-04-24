3dd27813aa7529b2693c8328615519d22f53c79f
Maciej Karczewski <m.karczewski1985@gmail.com>
2018-04-24 13:44:20 +0000

    Init

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,53 @@
+sap.ui.define(
+	[
+		"sap/ui/core/mvc/Controller",
+		"sap/m/MessageToast",
+		"opensap/myapp/model/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator"
+	],
+	function(Controller, MessageToast, formatter, Filter, FilterOperator) {
+
+		Controller.extend("opensap.myapp.controller.App", {
+			formatter: formatter,
+			onShowHello: function() {
+				// read msg from i18n model
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+				// show message
+				MessageToast.show(sMsg);
+			},
+
+			onFilterProducts: function(oEvent) {
+				// build filter array
+				var aFilter = [];
+				var sQuery = oEvent.getParameter("query");
+				// retrieve list control
+				var oList = this.getView().byId("productsList");
+				// get binding for aggregation 'items'
+				var oBinding = oList.getBinding("items");
+
+				if (sQuery) {
+					aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+				}
+				// apply filter. an empty filter array simply removes the filter
+				// which will make all entries visible again
+				oBinding.filter(aFilter);
+			},
+
+			onItemSelected: function(oEvent) {
+				var oSelectedItem = oEvent.getParameter("listItem");
+				var oContext = oSelectedItem.getBindingContext();
+				var sPath = oContext.getPath();
+				var oPanel = this.byId("productDetailsPanel");
+
+				oPanel.bindElement({
+					path: sPath
+				});
+				oPanel.setVisible(true);
+			}
+
+		});
+	});
---
+++ webapp/model/formatter.js
@@ -0,0 +1,21 @@
+sap.ui.define([], function() {
+	"use strict";
+	return {
+		delivery: function(sMeasure, iWeight) {
+			var sResult = "";
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			if (sMeasure === "G") {
+				iWeight = iWeight / 1000;
+			}
+			if (iWeight < 0.5) {
+				sResult = oResourceBundle.getText("formatterMailDelivery");
+			} else if (iWeight < 5) {
+				sResult = oResourceBundle.getText("formatterParcelDelivery");
+			} else {
+				sResult = oResourceBundle.getText("formatterCarrierDelivery");
+			}
+			return sResult;
+		}
+	};
+});
